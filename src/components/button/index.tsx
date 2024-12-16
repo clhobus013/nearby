616171a380c071e0ad015a351833c3df40e5881d
claudia <clauu013@gmail.com>
2024-12-16 16:42:04 -0300

    Cria componente para o botão

---
+++ src/components/button/index.tsx
@@ -0,0 +1,40 @@
+import { TouchableOpacity, Text, TextProps, TouchableOpacityProps, ActivityIndicator } from "react-native";
+
+import { IconProps as TablerIconProps } from "@tabler/icons-react-native"
+
+import { styles } from "./styles"
+import { colors } from "@/styles/theme"
+
+type ButtonProps = TouchableOpacityProps & {
+    isLoading?: boolean
+}
+
+function Button({children, style, isLoading = false, ...rest}: ButtonProps) {
+    return(
+        <TouchableOpacity 
+            style={[styles.container, style]}
+            activeOpacity={0.7}
+            disabled={isLoading}
+            {...rest}
+        >
+            { isLoading ? <ActivityIndicator size='small' color={colors.gray[100]}/> : children}
+        </TouchableOpacity>
+    )
+}
+
+function Title({children}: TextProps) {
+    return <Text style={styles.title}>{children}</Text>
+}
+
+type IconProps = {
+    icon: React.ComponentType<TablerIconProps>
+}
+
+function Icon({icon: Icon}:IconProps) {
+    return <Icon size={24} color={colors.gray[100]} />
+}
+
+Button.Title = Title
+Button.Icon = Icon
+
+export { Button }
