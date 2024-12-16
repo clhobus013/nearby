e5f3562e47ac8cb3e1c24a58b1413f7e77abce04
claudia <clauu013@gmail.com>
2024-12-16 16:42:18 -0300

    Finaliza tela de boas-vindas

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,15 @@
-import { View, Text } from "react-native"
+import { Button } from "@/components/button"
+import { Steps } from "@/components/steps"
+import { Welcome } from "@/components/welcome"
+import { IconPlus } from "@tabler/icons-react-native"
+import { View } from "react-native"
 
 export default function Index(){
     return (
-        <View>
-            <Text>Hello React Native</Text>
+        <View style={{flex: 1, padding: 40, gap: 40}}>
+            <Welcome/>
+            <Steps></Steps>
+            <Button><Button.Title>Começar</Button.Title></Button>
         </View>
     )
 }
