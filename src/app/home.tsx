4ed7901ef087d18856fe182fab0998d776375367
claudia <clauu013@gmail.com>
2025-01-08 15:39:44 -0300

    add tela home

---
+++ src/app/home.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+    return(
+        <View style={{ flex: 1 }}>
+        </View>
+    )
+}
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/button"
 import { Steps } from "@/components/steps"
 import { Welcome } from "@/components/welcome"
-import { IconPlus } from "@tabler/icons-react-native"
+import { router } from "expo-router"
 import { View } from "react-native"
 
 export default function Index(){
@@ -9,7 +9,9 @@ export default function Index(){
         <View style={{flex: 1, padding: 40, gap: 40}}>
             <Welcome/>
             <Steps></Steps>
-            <Button><Button.Title>Começar</Button.Title></Button>
+            <Button onPress={() => router.navigate("/home")}>
+                <Button.Title>Começar</Button.Title>
+            </Button>
         </View>
     )
 }
